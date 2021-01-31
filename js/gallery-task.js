a007fc04263c5facbdb89f557871eeab8d88bf15
Igor <Travis789@gmail.com>
2021-02-01 00:26:06 +0100

    first move

---
+++ js/gallery-task.js
@@ -0,0 +1,11 @@
+import imagesRef from "../gallery-items.js";
+const galleryList = document.querySelector('.js-gallery');
+
+const galleryString = imagesRef.reduce((acc, { preview, original, description }) => {
+    acc += `<li class="gallery__item"><a class="gallery__link" href="${original}"<img class="gallery__image"
+    src="${preview}" data-source="${original}" alt="${description}"/></a></li>`;
+
+    return acc; 
+}, '')
+
+galleryList.innerHTML += galleryString;

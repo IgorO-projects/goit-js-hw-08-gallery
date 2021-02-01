18a595289dcad3180235526df2efc19c94a8562b
Igor <Travis789@gmail.com>
2021-02-01 13:49:17 +0100

    toLeft&toRight-carousel-is-working

--- js/gallery-task.js
+++ js/gallery-task.js
@@ -7,9 +7,9 @@ const closeModalBtnRef = document.querySelector('button[data-action="close-light
 
 /* adding markup */ 
 
-const galleryString = imagesRef.reduce((acc, { preview, original, description }) => {
+const galleryString = imagesRef.reduce((acc, { preview, original, description }, index) => {
     acc += `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image"
-    src="${preview}" data-source="${original}" alt="${description}"/></a></li>`;
+    src="${preview}" data-source="${original}" alt="${description}" data-index="${index}"/></a></li>`;
     
     return acc; 
 }, '')
@@ -20,17 +20,22 @@ galleryList.innerHTML += galleryString;
 function openModal (event) {
     event.preventDefault();
     window.addEventListener('keydown', onESCPress);
+    window.addEventListener('keydown', onArrowLeftPress);
+    window.addEventListener('keydown', onArrowRightPress);
     const target = event.target;
     if (!target.classList.contains('gallery__image')) return;
     
     modalImageRef.src = target.dataset.source;
     modalImageRef.alt = target.alt;
+    modalImageRef.setAttribute('data-index', target.dataset.index);
 
     modalRef.classList.add('is-open');
 }
 
 function closeModal () {
     window.removeEventListener('keydown', onESCPress);
+    window.removeEventListener('keydown', onArrowLeftPress);
+    window.removeEventListener('keydown', onArrowRightPress);
     modalImageRef.src = '';
     modalImageRef.alt = '';
 
@@ -46,6 +51,38 @@ function closeModalViaOverlay (event) {
 function onESCPress (event) {
     if(event.code === 'Escape') {
         closeModal();
+    }
+}
+
+function onArrowRightPress (event) {
+    if (event.code === 'ArrowRight') {
+        const activeIndex = Number(modalImageRef.dataset.index);
+
+        if(activeIndex === imagesRef.length - 1) {
+            modalImageRef.src = imagesRef[imagesRef.length - 1].original;
+            modalImageRef.alt = imagesRef[imagesRef.length - 1].description;
+            modalImageRef.dataset.index = imagesRef.length - 1;
+        } else {
+            modalImageRef.src = imagesRef[activeIndex + 1].original;
+            modalImageRef.alt = imagesRef[activeIndex + 1].description;
+            modalImageRef.dataset.index = activeIndex + 1;
+        }
+    }
+}
+
+function onArrowLeftPress (event) {
+    if (event.code === 'ArrowLeft') {
+        const activeIndex = Number(modalImageRef.dataset.index);
+
+        if(activeIndex === 0) {
+            modalImageRef.src = imagesRef[0].original;
+            modalImageRef.alt = imagesRef[0].description;
+            modalImageRef.dataset.index = 0;
+        } else {
+            modalImageRef.src = imagesRef[activeIndex - 1].original;
+            modalImageRef.alt = imagesRef[activeIndex - 1].description;
+            modalImageRef.dataset.index = activeIndex - 1;
+        }
     }
 }
 

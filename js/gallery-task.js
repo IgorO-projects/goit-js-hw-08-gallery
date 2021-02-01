21bf2304a3a5a5b0159942db48a07c64f9a6e463
Igor <Travis789@gmail.com>
2021-02-01 01:19:18 +0100

    bag-has-been-fixed

--- js/gallery-task.js
+++ js/gallery-task.js
@@ -2,9 +2,9 @@ import imagesRef from "../gallery-items.js";
 const galleryList = document.querySelector('.js-gallery');
 
 const galleryString = imagesRef.reduce((acc, { preview, original, description }) => {
-    acc += `<li class="gallery__item"><a class="gallery__link" href="${original}"<img class="gallery__image"
+    acc += `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image"
     src="${preview}" data-source="${original}" alt="${description}"/></a></li>`;
-
+    
     return acc; 
 }, '')
 
